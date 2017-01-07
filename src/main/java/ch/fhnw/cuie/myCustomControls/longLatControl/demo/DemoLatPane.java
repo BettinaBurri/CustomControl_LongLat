ae2b3ab6c8393408e55f2a36d89307bb49bcf8ad
Bettina Burri <bettina.burri@students.fhnw.ch>
2017-01-07 22:52:51 +0100

    latitude Control added

---
+++ src/main/java/ch/fhnw/cuie/myCustomControls/longLatControl/demo/DemoLatPane.java
@@ -0,0 +1,109 @@
+package ch.fhnw.cuie.myCustomControls.longLatControl.demo;
+
+
+import ch.fhnw.cuie.myCustomControls.longLatControl.LatitudeControl;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
+
+
+/**
+ * @author Dieter Holz, Bettina Burri, Kathrin Koebel
+ */
+public class DemoLatPane extends BorderPane {
+    private LatitudeControl customControl;
+
+
+    private TextField   valueInputField;
+    private Slider      valueSlider;
+
+    private ColorPicker colorPicker;
+
+    public DemoLatPane() {
+        initializeControls();
+        layoutControls();
+        addBindings();
+    }
+
+    private void initializeControls() {
+        setPadding(new Insets(10));
+
+        customControl = new LatitudeControl();
+
+        valueInputField = new TextField();
+        valueInputField.setText("0.0");
+
+        // limit input to numbers & check min/max value
+        valueInputField.textProperty().addListener(new ChangeListener<String>() {
+            @Override public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (checkNumberRange(newValue, customControl.getMinLatValue(), customControl.getMaxLatValue()) && newValue.matches("^[0-9, -]*\\.?[0-9]*$")) {
+                    double value = parseSignedDouble(newValue);
+                } else {
+                    valueInputField.setText(oldValue);
+                }
+            }
+        });
+
+
+        valueSlider = new Slider(customControl.getMinLatValue(), customControl.getMaxLatValue(), 0);
+        valueSlider.setShowTickMarks(true);
+        //valueSlider.setShowTickLabels(true);
+
+        colorPicker = new ColorPicker();
+    }
+
+    private void layoutControls() {
+        setCenter(customControl);
+        VBox box = new VBox(10, new Label("Latitude"), valueInputField, valueSlider, colorPicker);
+        box.setPadding(new Insets(10));
+        box.setSpacing(10);
+        setRight(box);
+    }
+
+    private void addBindings() {
+        Bindings.bindBidirectional(valueInputField.textProperty(), customControl.valueProperty(), new NumberStringConverter());
+        valueSlider.valueProperty().bindBidirectional(customControl.valueProperty());
+
+        colorPicker.valueProperty().bindBidirectional(customControl.baseColorProperty());
+    }
+
+    private boolean checkNumberRange(String value, Double minValue, Double maxValue){
+        boolean positiveNumber = true;
+        if (value.startsWith("-")) {
+            value = value.substring(1);
+            positiveNumber = false;
+        }
+        double doubleValue = Double.parseDouble(value);
+
+        if(positiveNumber){
+            return doubleValue>=minValue && doubleValue<=maxValue;
+        }
+        else {
+            return -doubleValue>=minValue && -doubleValue<=maxValue;
+        }
+    }
+
+    private double parseSignedDouble(String value){
+        boolean positiveNumber = true;
+        if (value.startsWith("-")) {
+            value = value.substring(1);
+            positiveNumber = false;
+        }
+        double doubleValue = Double.parseDouble(value);
+
+        if(positiveNumber){
+            return doubleValue;
+        }
+        else {
+            return -doubleValue;
+        }
+    }
+}
--- src/main/java/ch/fhnw/cuie/myCustomControls/longLatControl/demo/DemoStarter.java
+++ src/main/java/ch/fhnw/cuie/myCustomControls/longLatControl/demo/DemoStarter.java
@@ -12,7 +12,7 @@ public class DemoStarter extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Region rootPanel = new DemoPane();
+        Region rootPanel = new DemoLatPane();
 
         Scene scene = new Scene(rootPanel);
 
