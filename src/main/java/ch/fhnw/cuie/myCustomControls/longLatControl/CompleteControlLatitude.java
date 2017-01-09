ed3b205bd31c4b3df8124b16fffd1df5a362d753
Bettina Burri <bettina.burri@students.fhnw.ch>
2017-01-09 16:50:03 +0100

    complete control added

---
+++ src/main/java/ch/fhnw/cuie/myCustomControls/longLatControl/CompleteControlLatitude.java
@@ -0,0 +1,15 @@
+package ch.fhnw.cuie.myCustomControls.longLatControl;
+
+import javafx.scene.control.TextField;
+
+import javax.xml.soap.Text;
+
+/**
+ * Created by bettina on 09.01.17.
+ */
+public class CompleteControlLatitude {
+
+    private TextField textField;
+
+
+}
